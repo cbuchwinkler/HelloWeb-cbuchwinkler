c709cdc2e6326d21dee33ddf4e421f769291c960
cbuchwinkler <43604671+cbuchwinkler@users.noreply.github.com>
2019-03-13 16:11:40 +0100

    intial commit

---
+++ Assets/Script/TestObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour {
+    string helloWorld ="Hello World";
+	public void PrintHelloWorld()
+    {
+        Debug.Log(helloWorld);
+    }
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
